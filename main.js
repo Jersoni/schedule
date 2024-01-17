853e342195b88e16af5880fafccf71f19a9de691
Jerson <jersoncaibog0423@gmail.com>
2024-01-18 01:37:48 +0800

    all

---
+++ main.js
@@ -0,0 +1,35 @@
+const date = new Date()
+const dateToday = document.querySelector('.date')
+const weekday = document.querySelector('.week-day')
+
+dateToday.textContent = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })
+weekday.textContent = date.toLocaleString('en', { weekday: 'long' })
+
+// date
+let column = ''
+column = weekday.textContent === 'Monday' || weekday.textContent === 'Wednesday' 
+    ? '1'
+    : '2'
+
+// time
+let row = ''
+
+const hour = date.getHours()
+const minute = date.getMinutes()
+
+if (hour < 9) {
+    row = '2'
+} else if (hour < 10 || (hour === 10 && minute < 30)) {
+    row = '3'
+} else if (hour < 13) {
+    row = '4'
+} else if (hour < 14 || (hour === 14 && minute < 30)) {
+    row = '5'
+} else if (hour < 15) {
+    row = '6'
+} else if (hour < 16) {
+    row = '7'
+}
+
+let subjectElement = document.querySelector(`.column-${column} > div:nth-of-type(${row})`)
+subjectElement.style.border = '1px solid rgba(0, 0, 0, .40)'
