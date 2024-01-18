f5a4d58de838c4fbdaaa352a0c400688d0d86646
Jerson <jersoncaibog0423@gmail.com>
2024-01-18 19:40:31 +0800

    ui adjustments

--- main.js
+++ main.js
@@ -5,31 +5,36 @@ const weekday = document.querySelector('.week-day')
 dateToday.textContent = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })
 weekday.textContent = date.toLocaleString('en', { weekday: 'long' })
 
+date.setHours(10)
+
 // date
-let column = ''
+let column = '1'
 column = weekday.textContent === 'Monday' || weekday.textContent === 'Wednesday' 
     ? '1'
     : '2'
 
 // time
-let row = ''
+let row = '1'
 
 const hour = date.getHours()
 const minute = date.getMinutes()
 
 if (hour < 9) {
-    row = '2'
+    row = '1'
 } else if (hour < 10 || (hour === 10 && minute < 30)) {
-    row = '3'
+    row = '2'
 } else if (hour < 13) {
-    row = '4'
+    row = '3'
 } else if (hour < 14 || (hour === 14 && minute < 30)) {
-    row = '5'
+    row = '4'
 } else if (hour < 15) {
+    row = '5'
+} else if (hour < 24) {
     row = '6'
-} else if (hour < 16) {
-    row = '7'
+} else {
+    console.log('error')
+    console.log(`${hour} : ${minute}`)
 }
 
 let subjectElement = document.querySelector(`.column-${column} > div:nth-of-type(${row})`)
-subjectElement.style.border = '1px solid rgba(0, 0, 0, .40)'
+subjectElement.style.border = '1px solid rgba(77, 150, 184, .5)'
